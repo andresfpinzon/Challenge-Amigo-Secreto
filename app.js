2db183b5fbbca62b2a045a0ebf34f625eb06da03
Andres Pinzon <andrespinzonxd@gmail.com>
2025-08-01 18:23:03 -0500

    se agrega la logica en ela rchivo app.js para agregar amigos a una lista, y sortear uno de los nombres

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+let lista = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value.trim();
+    if (amigo !== "") {
+        amigos.push(amigo);
+        document.getElementById("amigo").value = ""; // Limpiar el campo de entrada
+        mostrarAmigos();
+    } else {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+}
+
+function mostrarAmigos() {
+    
+    lista.innerHTML = ""; // Limpiar la lista antes de mostrar los amigos
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    
+    if (amigos.length > 0) {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+        lista.innerHTML = ""; // Limpiar la lista antes de mostrar los amigos
+        resultado.textContent = "El amigo sorteado es: " + amigoSorteado;
+    } else {
+        resultado.textContent = "No hay amigos para sortear.";
+    }
+}
